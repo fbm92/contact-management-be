af2262817adf240e20ada8d4c46bd55c2a84ef44
febrianmulawarman <febrianmulawarman92@gmail.com>
2025-08-11 19:15:06 +0800

    small change

--- src/middleware/error-middleware.ts
+++ src/middleware/error-middleware.ts
@@ -1,19 +1,33 @@
-import { NextFunction, Request, Response } from "express";
-import { ZodError } from "zod";
+import { NextFunction, Request, response, Response } from "express";
+import { array, ZodError } from "zod";
 import { ResponseError } from "../error/response-error";
+import { Prisma } from "@prisma/client";
 
-export const errorMiddleWare  = async (error : Error, req : Request, res : Response , next: NextFunction) => {
-    if(error instanceof ZodError) {
-        res.status(400).json({
-            errors : `Validation Error : ${JSON.stringify(error)}`
-        })
-    } else if (error instanceof ResponseError) {
-        res.status(error.status).json({
-            errors : error.message
-        })
-    } else {
-        res.status(500).json({
-            errors : error.message
-        })
+export const errorMiddleWare = async (error: Error, req: Request, res: Response, next: NextFunction) => {
+  if (error instanceof ZodError) {
+    const err = JSON.parse(error.message);
+    console.log(err);
+    
+    const result  = []
+    for (let i = 0; i < err.length; i++) {
+      result.push({
+        message : err[i].message,
+        path : err[i].path[0]
+      })
     }
-}
+    console.log(result);
+    
+    res.status(400).json({
+      errors: result,
+    });
+  }
+  else if (error instanceof ResponseError) {
+    res.status(error.status).json({
+      errors: error.message,
+    });
+  } else {
+    res.status(500).json({
+      errors: error.message,
+    });
+  }
+};
