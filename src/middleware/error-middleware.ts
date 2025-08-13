73e2af476088efbf0995f9bfe7c727d3022aa676
febrianmulawarman <febrianmulawarman92@gmail.com>
2025-08-13 10:00:39 +0800

    error rev1

--- src/middleware/error-middleware.ts
+++ src/middleware/error-middleware.ts
@@ -6,15 +6,17 @@ import { Prisma } from "@prisma/client";
 export const errorMiddleWare = async (error: Error, req: Request, res: Response, next: NextFunction) => {
   if (error instanceof ZodError) {
     const err = JSON.parse(error.message);
-    const result  = []
+    let result  = ""
     for (let i = 0; i < err.length; i++) {
-      result.push({
-        message : err[i].message,
-        path : err[i].path[0]
-      })
+      result += `Path : ${err[i].path[0]} : ${err[i].message} \n`
+      // result.push({
+      //   message : err[i].message,
+      //   path : err[i].path[0]
+      // })
     }
     res.status(400).json({
-      errors: JSON.stringify(result),
+      // errors: JSON.stringify(result),
+      errors : result
     });
   }
   else if (error instanceof ResponseError) {
